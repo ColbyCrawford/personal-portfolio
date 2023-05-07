f0a0c5d63e889cc4aa3210c550f5b7521fba64a7
Colby <colbyjcrawford@gmail.com>
2023-05-07 15:59:29 -0700

    tweaking for tablet devices

--- js/theme.js
+++ js/theme.js
@@ -1,10 +1,17 @@
 const themeToggle = document.getElementById("theme-toggle");
 const htmlEl = document.querySelector("html");
 
+toggleThemes();
+
 themeToggle.addEventListener("click", e => {
+    toggleThemes();
+})
+
+
+function toggleThemes() {
     if(themeToggle.checked) {
         htmlEl.classList.add("theme-dark");
     } else {
         htmlEl.classList.remove("theme-dark");
     }
-})
+}
